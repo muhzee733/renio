f870ed03e4f5d9765c90ab02bdbcd71999a15a7a
basit <basatali326@gmail.com>
2025-02-15 12:26:30 +0400

    added

--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -1,82 +1,93 @@
-import React,{useRef} from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Autoplay } from "swiper/modules";
-import { Pagination } from "swiper/modules";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
-const Section6 = () => {
+function Section6() {
+  const sectionRef = useRef();
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      import("lenis").then(({ default: Lenis }) => {
+        const lenis = new Lenis();
+        function raf(time) {
+          lenis.raf(time);
+          requestAnimationFrame(raf);
+        }
+        requestAnimationFrame(raf);
+      });
+
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top top",
+          end: "+=500%",
+          scrub: 1,
+          pin: true,
+        },
+      });
+
+      tl.to(".software-right-container", {
+        xPercent: -105 * 3,
+        ease: "none",
+      });
+
+      return () => {
+        ScrollTrigger.getAll().forEach((st) => st.kill());
+      };
+    }
+  }, []);
+
   return (
-    <>
-      <div className=" bg-white software">
-        <div className="container">
-          <div class="row">
-            <div class="col-lg-6 col-md-12 d-flex align-items-center">
-              <div className="software-left d-flex gap-4 w-100 align-items-center">
-                <img src="/assets/re-logo.png" width="auto" />
-                <div className="">
-                  <h2>Renie</h2>
-                  <h6>Software</h6>
-                  <p>Where everything comes together</p>
-                </div>
-              </div>
+    <main className="bg-[#ffffff] w-full overflow-hidden">
+      <section ref={sectionRef} className="flex min-h-screen w-full">
+        <div className="w-1/2 flex justify-center items-center software-left">
+          <img src="/assets/re-logo.png" width="auto" />
+          <div>
+            <h2>Renie</h2>
+            <h6>Software</h6>
+            <p>Where everything comes together</p>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex items-center overflow-hidden relative">
+          <div
+            className="flex flex-nowrap space-x-10"
+          >
+            <div className="software-right-container">
+              <img src="/assets/renie-hub.png" />
+              <span>Renie Admin</span>
+              <p>
+                Renie Admin centralizes all management tasks, providing seamless control over operations, data, and user interactions.
+              </p>
             </div>
-            <div class="col-lg-6 col-md-12">
-              <div className="software-right-column">
-                <Swiper
-                  slidesPerView={1}
-                  autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                  }}
-                  gap={30}
-                  modules={[Autoplay, Pagination]}
-                  pagination={{ clickable: true }}
-                >
-                  <SwiperSlide>
-                    <div className="software-right-container">
-                      <img src="/assets/renie-hub.png" />
-                      <span>Renie Admin</span>
-                      <p>
-                      Renie Admin centralizes all management tasks, providing seamless control over operations, data, and user interactions.
-                      </p>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="software-right-container">
-                      <img src="/assets/renie-hub-2.png" />
-                      <span>Renie Hub</span>
-                      <p>
-                      Our innovative platform makes it possible to generate a passive income from data monetization of plastic waste
-                      </p>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="software-right-container">
-                      <img src="/assets/renie-hub-3.png" />
-                      <span>Renie Rewards</span>
-                      <p>
-                      Renie Nexus makes recycling easy with instant access and prize rewards, boosting sustainability.
-                      </p>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="software-right-container">
-                      <img src="/assets/renie-hub-4.png" />
-                      <span>Renie Ads</span>
-                      <p>
-                      Our AI platform uses Renie Bin data to deliver targeted ads, driving eco-friendly engagement.
-                      </p>
-                    </div>
-                  </SwiperSlide>
-                </Swiper>
-              </div>
+            <div className="software-right-container">
+              <img src="/assets/renie-hub-2.png" />
+              <span>Renie Hub</span>
+              <p>
+                Our innovative platform makes it possible to generate a passive income from data monetization of plastic waste.
+              </p>
+            </div>
+            <div className="software-right-container">
+              <img src="/assets/renie-hub-3.png" />
+              <span>Renie Rewards</span>
+              <p>
+                Renie Nexus makes recycling easy with instant access and prize rewards, boosting sustainability.
+              </p>
+            </div>
+            <div className="software-right-container">
+              <img src="/assets/renie-hub-4.png" />
+              <span>Renie Ads</span>
+              <p>
+                Our AI platform uses Renie Bin data to deliver targeted ads, driving eco-friendly engagement.
+              </p>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </section>
+    </main>
   );
-};
+}
 
 export default Section6;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -18,6 +18,7 @@ export default function Document() {
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           strategy="beforeInteractive"
         />
+        <Script src="https://cdn.tailwindcss.com" />
       </body>
     </Html>
   );
