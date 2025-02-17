6ea48ec33f5aefa68b7a35db4409301150b506c5
basit <basatali326@gmail.com>
2025-02-17 17:30:32 +0400

    footer is pending

--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -2,89 +2,89 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <div className="footer-section bg-white">
-        <div class="container">
-          <div className="footer-wrapper">
-            <div className="footer-upper-section">
-              <div className="footer-left-section">
-                <h6>Sign up to receive the latest news and support articles</h6>
-                <div className="newsletter">
-                  <input type="email" placeholder="Enter your email address" />
-                  <button>Sign Up Now</button>
+    <div className="footer-section">
+      <div class="footer-main-section">
+        <div className="footer-wrapper">
+          <div className="container">
+            <div class="container-wrapper">
+              <div class="row">
+                <div class="col-lg-6">
+                  <div className="footer-left-section">
+                    <h6>
+                      Sign up to receive the latest news and support articles
+                    </h6>
+                    <div className="newsletter">
+                      <input
+                        type="email"
+                        placeholder="Enter your email address"
+                      />
+                      <button>Sign Up Now</button>
+                    </div>
+                    <p className="footer-text">
+                      by submitting this form, you agree to our{" "}
+                      <span className="text-bold">privacy policy</span>.
+                    </p>
+                  </div>
                 </div>
-                <p className="footer-text">
-                  by submitting this form, you agree to our{" "}
-                  <span className="text-bold">privacy policy</span>.
-                </p>
+                <div class="col-lg-6">
+                  <div class="row">
+                    <div class="col-lg-4">
+                      <div className="userfull-links">
+                        <h6>Pages</h6>
+                        <ul>
+                          <li>Renie Bin</li>
+                          <li>Renie Hub </li>
+                          <li>Renie App </li>
+                          <li>Ads that matter</li>
+                          <li>Media Hub</li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-lg-4">
+                      <div className="userfull-links">
+                        <h6>Legals</h6>
+                        <ul>
+                          <li>Privacy policy</li>
+                          <li>Platform terms</li>
+                          <li>Terms of use</li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-lg-4">
+                      <div className="userfull-links">
+                        <h6>Follow us</h6>
+                        <ul>
+                          <li className="d-flex gap-2">
+                            <img src="/assets/facebook.png" />
+                            <span>Facebook</span>
+                          </li>
+                          <li className="d-flex gap-2">
+                            <img src="/assets/linkedin.png" />
+                            <span>Linkedin</span>
+                          </li>
+                          <li className="d-flex gap-2">
+                            <img src="/assets/instragram.png" />
+                            <span>Instagram</span>
+                          </li>
+                          <li className="d-flex gap-2">
+                            <img src="/assets/tiktok.png" />
+                            <span>Tiktok</span>
+                          </li>
+                          <li className="d-flex gap-2">
+                            <img src="/assets/youtube.png" />
+                            <span>Youtube</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="foolter-links">
-                <div className="userfull-links">
-                  <h6>Pages</h6>
-                  <ul>
-                    <li>Renie Bin</li>
-                    <li>Renie Hub </li>
-                    <li>Renie App </li>
-                    <li>Ads that matter</li>
-                    <li>Media Hub</li>
-                  </ul>
-                </div>
-                <div className="userfull-links">
-                  <h6>Legals</h6>
-                  <ul>
-                    <li>Privacy policy</li>
-                    <li>Platform terms</li>
-                    <li>Terms of use</li>
-                  </ul>
-                </div>
-                <div className="userfull-links">
-                  <h6>Follow us</h6>
-                  <ul>
-                    <li>
-                      <span style={{ marginRight: "10px" }}>
-                        <img src="/assets/facebook.png" />
-                      </span>
-                      Facebook
-                    </li>
-                    <li>
-                      <span style={{ marginRight: "10px" }}>
-                        <img src="/assets/linkedin.png" />
-                      </span>
-                      Linkedin
-                    </li>
-                    <li>
-                      <span style={{ marginRight: "10px" }}>
-                        <img src="/assets/instragram.png" />
-                      </span>
-                      Instagram
-                    </li>
-                    <li>
-                      <span style={{ marginRight: "10px" }}>
-                        <img src="/assets/tiktok.png" />
-                      </span>
-                      Tiktok
-                    </li>
-                    <li>
-                      <span style={{ marginRight: "10px" }}>
-                        <img src="/assets/youtube.png" />
-                      </span>
-                      Youtube
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="footer-down-section">
-              <img src="/assets/footer-text.png" alt="footer" />
             </div>
           </div>
         </div>
       </div>
-      <div className="bootom-footer">
-        <p>© 2024 All rights reserved. Renie Tech Trading LLC</p>
-        <p>Country : Qatar | Saudi Arabia | United Arab Emirates</p>
-      </div>
-    </>
+    </div>
   );
 };
 
