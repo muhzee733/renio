92c0468dc87ad847f0b62d4c13b4afea696ffb1a
basit <basit@renie.io>
2025-03-03 21:41:54 +0400

    added

--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -2,14 +2,22 @@ import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
+  const pages = [
+    { name: "Renie Bin", path: "/reniebin" },
+    { name: "Renie Hub", path: "/reniehub" },
+    { name: "Renie App", path: "/renieapp" },
+    { name: "Ads that Matter", path: "/renieads" },
+    { name: "Media Hub", path: "/mediahub" },
+  ];
+
   return (
     <div className="footer-section">
-      <div class="footer-main-section">
+      <div className="footer-main-section">
         <div className="footer-wrapper">
           <div className="container p-0">
-            <div class="container-wrapper">
-              <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-12">
+            <div className="container-wrapper">
+              <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12">
                   <div className="footer-left-section">
                     <h6>
                       Sign up to receive the latest news and support articles
@@ -22,99 +30,102 @@ const Footer = () => {
                       <button>Sign Up Now</button>
                     </div>
                     <p className="footer-text">
-                      by submitting this form, you agree to our{" "}
+                      By submitting this form, you agree to our{" "}
                       <span className="text-bold">privacy policy</span>.
                     </p>
                   </div>
                 </div>
-                <div class="col-lg-6 col-md-6">
-                  <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col">
+                <div className="col-lg-6 col-md-6">
+                  <div className="row">
+                    {/* Pages Section */}
+                    <div className="col-lg-4 col-md-4 col-sm-4">
                       <div className="userfull-links">
                         <h6>Pages</h6>
                         <ul>
-                          <li>Renie Bin</li>
-                          <li>Renie Hub </li>
-                          <li>Renie App </li>
-                          <li>Ads that matter</li>
-                          <li>Media Hub</li>
+                          {pages.map((page, index) => (
+                            <li key={index}>
+                              <Link href={page.path}>{page.name}</Link>
+                            </li>
+                          ))}
                         </ul>
                       </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 col">
+
+                    {/* Legals Section */}
+                    <div className="col-lg-4 col-md-4 col-sm-4">
                       <div className="userfull-links">
                         <h6>Legals</h6>
                         <ul>
-                          <Link href="/privacypolicy">
-                            <li>Privacy policy</li>
-                          </Link>
-                          <Link href="/platform-terms">
-                            <li>Platform terms</li>
-                          </Link>
-                          <Link href="/term-of-services">
-                            <li>Terms of services</li>
-                          </Link>
+                          <li>
+                            <Link href="/privacypolicy">Privacy policy</Link>
+                          </li>
+                          <li>
+                            <Link href="/platform-terms">Platform terms</Link>
+                          </li>
+                          <li>
+                            <Link href="/term-of-services">Terms of services</Link>
+                          </li>
                         </ul>
                       </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 col">
+
+                    {/* Social Media Section */}
+                    <div className="col-lg-4 col-md-4 col-sm-4">
                       <div className="userfull-links">
                         <h6>Follow us</h6>
                         <ul>
-                          <Link href="https://www.facebook.com/wearerenie">
-                            <li className="d-flex gap-2">
-                              <img src="/assets/facebook.png" />
+                          <li>
+                            <Link href="https://www.facebook.com/wearerenie">
                               <span>Facebook</span>
-                            </li>
-                          </Link>
-                          <Link href="https://www.linkedin.com/company/renietech">
-                          <li className="d-flex gap-2">
-                            <img src="/assets/linkedin.png" />
-                            <span>Linkedin</span>
+                            </Link>
                           </li>
-                          </Link>
-                          <Link href="https://www.instagram.com/wearerenie/">
-                          <li className="d-flex gap-2">
-                            <img src="/assets/instragram.png" />
-                            <span>Instagram</span>
+                          <li>
+                            <Link href="https://www.linkedin.com/company/renietech">
+                              <span>LinkedIn</span>
+                            </Link>
                           </li>
-                          </Link>
-                          
-                          <Link href="https://www.tiktok.com/@wearerenie">
-                          <li className="d-flex gap-2">
-                            <img src="/assets/tiktok.png" />
-                            <span>Tiktok</span>
-                          </li></Link>
-                          
-                          <Link href="https://www.youtube.com/@wearerenie">
-                          <li className="d-flex gap-2">
-                            <img src="/assets/youtube.svg" />
-                            <span>Youtube</span>
-                          </li></Link>
-                         
+                          <li>
+                            <Link href="https://www.instagram.com/wearerenie/">
+                              <span>Instagram</span>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link href="https://www.tiktok.com/@wearerenie">
+                              <span>TikTok</span>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link href="https://www.youtube.com/@wearerenie">
+                              <span>YouTube</span>
+                            </Link>
+                          </li>
                         </ul>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="row">
-                <div class="col-lg-12">
+
+              {/* Footer Logo Section */}
+              <div className="row">
+                <div className="col-lg-12">
                   <div className="custom-center">
                     <img src="/assets/footer-text.png" alt="renio" />
                   </div>
                 </div>
               </div>
             </div>
-            <div class="row bootom-footer">
-              <div class="col-lg-6 col-md-6">
+
+            {/* Bottom Footer */}
+            <div className="row bootom-footer">
+              <div className="col-lg-6 col-md-6">
                 <p className="text-left">
                   © 2024 All rights reserved. Renie Tech Trading LLC
                 </p>
               </div>
-              <div class="col-lg-6 col-md-6">
+              <div className="col-lg-6 col-md-6">
                 <p className="text-right">
-                  Country : Qatar | Saudi Arabia | United Arab Emirates
+                  Country: Qatar | Saudi Arabia | United Arab Emirates
                 </p>
               </div>
             </div>
