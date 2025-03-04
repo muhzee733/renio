c25ea1e41470ec6fbf06335a099eb4e37dad0cbd
basit <basit@renie.io>
2025-03-04 14:46:37 +0400

    added

--- src/components/RenieAdmin/ThirdSection.jsx
+++ src/components/RenieAdmin/ThirdSection.jsx
@@ -2,16 +2,22 @@ import React from "react";
 
 const ThirdSection = () => {
   return (
-    <section className="dashboard-preview text-center bg-white">
-      <div className="container">
+    <section className="dashboard-preview text-center bg-white ">
+      <div className="container ">
         <h2 className="text-primary">Powerful Admin Panel</h2>
-        <figure>
+        <figure className="position-relative">
           <img
             src="/assets/renieadmin/image.png"
             alt="Dashboard Preview"
             className="img-fluid mb-4"
           />
+          <img
+            src="/assets/renieadmin/logo.png"
+            alt="Dashboard Preview"
+            className="img-fluid custom-ads-image"
+          />
         </figure>
+        
         <h2 className="text-primary">For Franchise Success!</h2>
         <p className="mb-4">
           Simplify operations, enhance communication, and track performance—all
