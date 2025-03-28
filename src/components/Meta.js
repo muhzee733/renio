05f37934505d3e6101ffd990eee8cf5e755bf21a
Basitali326 <basit@renie.io>
2025-03-28 10:48:35 +0400

    added

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -5,8 +5,9 @@ const Meta = ({ title, description }) => {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-      <meta charset="utf-8"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.webp" sizes="any" />
     </Head>
   );
 };
