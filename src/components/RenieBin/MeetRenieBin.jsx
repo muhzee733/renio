74a0036a99887475623157ec818facc4a31f2f2f
Basitali326 <basit@renie.io>
2025-04-03 15:37:34 +0400

    added

--- src/components/RenieBin/MeetRenieBin.jsx
+++ src/components/RenieBin/MeetRenieBin.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
+import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
-import H2 from "../Typography/H2";
-import P from "../Typography/P";
+import H4 from "../Typography/H4";
 
 const Model = ({ path }) => {
   const { scene } = useGLTF(path);
-  return <primitive object={scene} position={[0, -1, 0]} />;
+  return <primitive object={scene} position={[0, 0, 0]} rotation={[0, 0.6, 0]} />;
 };
 
 const MeetRenieBin = () => {
+  const images = [
+    "/assets/2.png",
+    "/assets/5.png",
+    "/assets/3.png",
+    "/assets/4.png",
+  ];
+  const titles = ["RENIE ONE", "RENIE TWO", "RENIE MAXI", "RENIE MINI"];
   const models = [
     "/assets/test.glb",
     "/assets/two-way.glb",
@@ -21,99 +23,62 @@ const MeetRenieBin = () => {
     "/assets/four-way.glb",
   ];
 
+  const [selectedModel, setSelectedModel] = useState(null);
+
   return (
     <div className="MeetRenieBin">
       <div className="container-xl">
-        <div className="row">
+        <div className="row text-center">
           <div className="col-lg-12">
-            <div className="meet-main-banner">
-              <H2 title={'Meet Renie Bins'} />
-              <P title={'Collect multiple waste types in one bin'} />
-            </div>
-            <div className="meet-image">
-              <Swiper
-                modules={[Pagination]}
-                spaceBetween={30}
-                slidesPerView={1}
-                pagination={{ clickable: true }}
-                breakpoints={{
-                  300: { slidesPerView: 1 },
-                  768: { slidesPerView: 3 },
-                  1024: { slidesPerView: 4 },
-                }}
-              >
-                {models.map((model, index) => (
-                  <SwiperSlide key={index}>
-                    <Canvas style={{ height: "400px", width: "350px" }}>
-                      <ambientLight intensity={1.2} />
-                      <directionalLight position={[2, 2, 2]} intensity={2} />
-                      <Model path={model} />
-                      <OrbitControls
-                        enableZoom={false}
-                        minDistance={30}
-                        maxDistance={3}
-                      />
-                    </Canvas>
-                    <h3 className="model-title">
-                      {
-                        [
-                          "RENIE ONE",
-                          "RENIE TWO",
-                          "RENIE MAXI",
-                          "RENIE MINI",
-                        ][index]
-                      }
-                    </h3>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-            <div className="reniebin-paragraph">
-              <p className="renie-bin-paragraph">
-                Renie Bins are equipped with special sensors that mimic a
-                vision-type experience, eliminating the need for expensive
-                processing power. This cutting-edge technology ensures the bins
-                remain as affordable as traditional, non-smart bins. Enjoy the
-                benefits of smart waste management without the high costs,
-              </p>
-              <div className="renie-bin-custom">
-                <div>
-                  <img src="/assets/Vector.webp" />
-                  <p>Plastic bottles</p>
+            <div className="meet-image d-flex justify-content-center align-items-center gap-4 flex-wrap">
+              {images.map((img, index) => (
+                <div
+                  key={index}
+                  className="image-container text-center"
+                  onClick={() => setSelectedModel(models[index])}
+                  style={{ cursor: "pointer" }}
+                >
+                  <img src={img} alt={titles[index]} className="static-image" />
+                  <H4 title={titles[index]} />
                 </div>
-                <div>
-                  <img src="/assets/Vector-2.webp" />
-                  <p>Plastic bups</p>
-                </div>
-                <div>
-                  <img src="/assets/Vector-3.webp" />
-                  <p>Paper bups</p>
-                </div>
-                <div>
-                  <img src="/assets/Vector-4.webp" />
-                  <p>Cans</p>
-                </div>
-                <div>
-                  <img src="/assets/Vector-5.webp" />
-                  <p>Carton</p>
-                </div>
-                <div>
-                  <img src="/assets/Group-icon-1.png" />
-                  <p>Vape</p>
-                </div>
-                <div>
-                  <img src="/assets/Group-icon-2.png" />
-                  <p>Battery</p>
-                </div>
-                <div>
-                  <img src="/assets/Group-icon-3.png" />
-                  <p>E-Waste</p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
+
+      {selectedModel && (
+        <div className="model-popup" id="custom-popup">
+          <div className="model-popup-content">
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                setSelectedModel(null);
+              }}
+              className="close-btn"
+              style={{
+                position: "absolute",
+                top: "10px",
+                right: "10px",
+                color: "black",
+                border: "none",
+                cursor: "pointer",
+                padding: "5px 10px",
+                fontSize: "16px",
+                zIndex: "999",
+              }}
+            >
+              ✖
+            </button>
+            <Canvas style={{ height: "500px", width: "500px" }}>
+              <ambientLight intensity={1.2} />
+              <directionalLight position={[2, 2, 2]} intensity={2} />
+              <Model path={selectedModel} />
+              <OrbitControls enableZoom={true} minDistance={30} />
+            </Canvas>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,7 +15,7 @@ export default function Document() {
         <NextScript />
         
         <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           strategy="beforeInteractive"
         />
         <Script src="https://cdn.tailwindcss.com" />
