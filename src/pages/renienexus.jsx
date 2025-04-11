d2048e642ac7b78b682d12b9afdcdcdee60a9551
Basitali326 <basit@renie.io>
2025-04-11 11:00:34 +0400

    added

--- src/pages/renienexus.jsx
+++ src/pages/renienexus.jsx
@@ -1,7 +1,18 @@
+import Meta from "@/components/Meta";
+import Banner from "@/components/RenieApp/Banner";
 import React from "react";
 
 const RenieNexus = () => {
-  return <div>RenieNexus</div>;
+  return (
+    <>
+      <Meta
+        title="Renie App: Track Your Recycling Impact Anytime, Anywhere"
+        description="Download the Renie App to easily track your recycling impact, find nearby bins, and earn rewards for your sustainable efforts."
+      />
+      <Banner />
+      
+    </>
+  );
 };
 
 export default RenieNexus;
