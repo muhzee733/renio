0e4402f2f78076ad5d9f7b22f3d5754ecb229377
Basitali326 <basit@renie.io>
2025-04-15 09:27:09 +0400

    added

--- src/pages/renienexus.jsx
+++ src/pages/renienexus.jsx
@@ -1,33 +1,9 @@
-import Meta from "@/components/Meta";
-import Footer from "@/components/Navbar/Footer";
-import MiniFooter from "@/components/Navbar/MiniFooter";
-import Navbar from "@/components/Navbar/Navbar";
-import AnimatedSection from "@/components/RenieApp/AnimatedSection";
-import Banner from "@/components/RenieApp/Banner";
-import EasyStep from "@/components/RenieApp/EasyStep";
-import Green from "@/components/RenieApp/Green";
-import LoveRenie from "@/components/RenieApp/LoveRenie";
-import RenieNexusApp from "@/components/RenieApp/RenieNexusApp";
-import React from "react";
+import React from 'react'
 
 const RenieNexus = () => {
   return (
-    <>
-      <Meta
-        title="Renie App: Track Your Recycling Impact Anytime, Anywhere"
-        description="Download the Renie App to easily track your recycling impact, find nearby bins, and earn rewards for your sustainable efforts."
-      />
-      <Navbar />
-      <Banner />
-      <RenieNexusApp />
-      <LoveRenie />
-      <EasyStep />
-      <AnimatedSection />
-      <Green />
-      <Footer />
-      <MiniFooter />
-    </>
-  );
-};
+    <div>RenieNexus</div>
+  )
+}
 
-export default RenieNexus;
+export default RenieNexus
